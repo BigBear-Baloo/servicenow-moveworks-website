1a64c3b2d8066b96d1aa610b4a7b8f1344fcf9f6
BigBear-Baloo <jhfahrenkamp@gmail.com>
2025-03-24 00:19:22 +0100

    scripts.js

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Website Loaded: ServiceNow Acquisition of Moveworks');
     // Future interactive functionalities can be added here
 });
+
+function toggleCard(cardElement) {
+    cardElement.classList.toggle('open');
+}
